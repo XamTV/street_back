31e207c9cfc72a7f642c40123997af24e78f4c59
Maxime <maxime@siruplab.com>
2025-06-16 19:27:08 +0200

    docker fix

--- server/app/config.js
+++ server/app/config.js
@@ -4,7 +4,6 @@ const express = require("express");
 
 const cors = require("cors");
 
-const path = require("path");
 
 const app = express();
 
@@ -109,25 +108,9 @@ app.use("/api", apiRouter);
 
 // Code a commenter pour faire marcher les images
 
-const reactBuildPath = path.join(__dirname, "/../../client/dist");
-const publicFolderPath = path.join(__dirname, "/../public");
-
-// Serve react resources
-app.use(express.static(reactBuildPath));
-
-// Serve server resources
-app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
-
-// Redirect unhandled requests to the react index file
 app.get("*", (req, res) => {
-  if (req.path.includes("public")) {
-    const urlpath = req.path.split("/");
-    res.sendFile(path.join(__dirname, "..", "public", urlpath[2], urlpath[3]));
-  } else {
-    res.sendFile(path.join(reactBuildPath, "index.html"));
-  }
+  res.status(404).send("Not Found");
 });
-
 /* ***************************************.********************************** */
 
 // Middleware for Error Logging (Uncomment to enable)
